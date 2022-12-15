e21b9fd2bfe58b3dfcc953b95818e35e34743bce
sselat <tales.beraldo@gmail.com>
2022-12-15 15:59:17 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 function checkInputs() {
-    const formInputs = document.getElementsByClassName('inputData');
-    console.log(`Inputzinho`)
+    const formInputs = document.querySelectorAll('.inputData');
+    const inputDiv = document.querySelectorAll('.inputDiv');
 
-
-    for(let i = 0; i < formInputs.length ; i++){
-        console.log(`Inputzinho ${i}`)
+    console.log('ativou!')
+    formInputs.forEach(toggleClasses);
+    function toggleClasses (item, index) {
+        console.log(index)
+        if (item.value !== '') {
+            item.classList.remove('emptyInput')
+            inputDiv[index].classList.remove('emptyInput')
+            item.classList.add('validInput')
+        } else {
+            item.classList.add('emptyInput')
+            inputDiv[index].classList.add('emptyInput')
+            item.classList.remove('validInput')
+        }
     }
 }
